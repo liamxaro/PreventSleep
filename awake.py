d4242e6f1579e94306439093e42eacdbe9ceefda
laro <ljaro11@comcast.net>
2023-10-10 16:14:09 -0400

    Added variable to adjust time we want the script to prevent sleep

--- awake.py
+++ awake.py
@@ -7,6 +7,8 @@ def on_mouse_move(x, y):
 def main():
     initialMousePos = pyautogui.position()
     startTime = time.time()
+    timeComparison = 240
+
 
     while True:
         currentMousePos = pyautogui.position()
@@ -14,7 +16,7 @@ def main():
             on_mouse_move(*currentMousePos)
             initialMousePos = currentMousePos
             startTime = time.time()
-        elif int(time.time() - startTime) == 3:
+        elif int(time.time() - startTime) == timeComparison:
              pyautogui.moveTo(200, 200, duration=0.10)
              pyautogui.moveTo(200, 800, duration=0.10)
              pyautogui.moveTo(1275, 800, duration=0.10)
