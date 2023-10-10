918ed95e1465e334dda7c466f148b6c57364ac4a
laro <ljaro11@comcast.net>
2023-10-10 15:12:02 -0400

    Initial commit

---
+++ awake.py
@@ -0,0 +1,32 @@
+import pyautogui
+import time
+
+def on_mouse_move(x, y):
+    print(f"Mouse moved to ({x}, {y}).")
+
+def main():
+    initialMousePos = pyautogui.position()
+    startTime = time.time()
+
+    while True:
+        currentMousePos = pyautogui.position()
+        if currentMousePos != initialMousePos:
+            on_mouse_move(*currentMousePos)
+            initialMousePos = currentMousePos
+            startTime = time.time()
+        elif int(time.time() - startTime) == 3:
+             pyautogui.moveTo(200, 200, duration=0.10)
+             pyautogui.moveTo(200, 800, duration=0.10)
+             pyautogui.moveTo(1275, 800, duration=0.10)
+             pyautogui.moveTo(1275, 200, duration=0.10)
+             pyautogui.moveTo(200, 200, duration=0.10)
+             pyautogui.keyDown('shift')
+             pyautogui.keyUp('shift')
+             initialMousePos = pyautogui.position()
+             startTime = time.time()
+
+
+   
+
+
+main()
